d357cdc520a31470cec1788e50bfefac1b6ad3f8
Virad01 <iamvirad09@gmail.com>
2022-05-18 05:52:49 +0530

    13 solved

--- union-intersection.cpp
+++ union-intersection.cpp
@@ -118,7 +118,7 @@ int printIntersection(int arr1[], int arr2[], int m, int n)
        i++; 
     else if (arr2[j] < arr1[i]) 
        j++; 
-    else /* if arr1[i] == arr2[j] */
+    else /* if arr1[i] == arr2[j] */ 
     { 
        cout << arr2[j] << " "; 
        i++; 
