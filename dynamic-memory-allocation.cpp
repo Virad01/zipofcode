547e741b86e4f40db0f569798d12b86a33e3179d
Virad01 <iamvirad09@gmail.com>
2022-05-25 23:32:16 +0530

    Update dynamic-memory-allocation.cpp

--- dynamic-memory-allocation.cpp
+++ dynamic-memory-allocation.cpp
@@ -13,12 +13,25 @@ address pointer me store hota hai
 to create an array dynamically  
 
 int * arr = new int[n]
+new int will return the address  
+
+memory leak----- dynamic memory bani heap me aur pointer bana stack me aur agar dono ke bich ka connection toot jata hai 
+
+heap ki memory automatically kyun nii destory hoti??
+
+answer-- heap ke andar sab explicitily hota hai 
+
 
 
 refernce variable ----same memory different names
 
 
 we can create a reference vatriable int &j = i;
+
+
+stack memory is always choti than heap memory  
+
+{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{{MACROS}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}}
 
 
 
