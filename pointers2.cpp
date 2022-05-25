08e51798aa5b2e7048bae49196083a5d13fa7f13
Virad01 <iamvirad09@gmail.com>
2022-05-25 06:55:01 +0530

    Update pointers2.cpp

--- pointers2.cpp
+++ pointers2.cpp
@@ -2,6 +2,21 @@
 //Double Pointers 
 //*q means value of address pointed by q 
 //how many double pointer we can create
+/* 
+
+arr+1 is case me app ek integer aage badhte hai 
+&arr + 1 is case me aap ek array aage badte hai 
+
+
+
+
+
+
+
+
+
+
+ */
 #include<iostream>
 
 using namespace std;
